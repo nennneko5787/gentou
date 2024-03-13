a9f7a0940922707e1801c5e4468d7fadea667109
nennneko5787 <nennneko5787@gmail.com>
2024-03-13 19:55:37 +0900

    おおおお

--- main.py
+++ main.py
@@ -3,6 +3,7 @@ from discord.ext import tasks
 from datetime import datetime
 import os
 from keep_alive import keep_alive
+from zoneinfo import ZoneInfo
 
 client = discord.Client(intents=discord.Intents.default())
 JIHOU_CHANNEL_ID = 1208730354656084008 #チャンネルID
@@ -20,7 +21,7 @@ async def on_ready():
 @tasks.loop(seconds=60)
 async def loop():
     # 現在の時刻
-    now = datetime.now().strftime('%H:%M')
+    now = datetime.now(ZoneInfo("Asia/Tokyo")).strftime('%H:%M')
     if now == '00:00':
         channel = client.get_channel(JIHOU_CHANNEL_ID)
         await channel.send('https://cdn.discordapp.com/attachments/1208773839023247390/1216853798086246471/AP1GczNYgiHoE-mNro7tzg-rB65rTJ-CfPC98xw2hDvNFzRx6RDVs794Xee_Www1200-h675-s-no.png?ex=6601e669&is=65ef7169&hm=bc216034890394dc50482395cb8b776b5bf7f6257da343a50b8133abff2b739a&')
