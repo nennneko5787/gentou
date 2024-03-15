08d6491fbcac8572bcd92d55e05c9b0f7ddd7f34
nennneko5787 <nennneko5787@gmail.com>
2024-03-15 11:45:10 +0900

    https://discord.com/channels/1208388325954560071/1208388352810946611/1218021432672391248 motiron jissou siteokimasita

--- main.py
+++ main.py
@@ -1,11 +1,14 @@
 import discord
+from discord.ext import app_commands
 from discord.ext import tasks
 from datetime import datetime
 import os
 from keep_alive import keep_alive
 from zoneinfo import ZoneInfo
+import psutil
 
 client = discord.Client(intents=discord.Intents.default())
+tree = app_commands.CommandTree(client=client)
 JIHOU_CHANNEL_ID = 1208730354656084008 #チャンネルID
 
 @client.event
@@ -14,8 +17,18 @@ async def setup_hook():
 
 @client.event
 async def on_ready():
+    await tree.sync()
     print("起動!")
     await client.change_presence(activity=discord.Game(name="幻塔"))
+
+@tree.command(name="ping", description="pingを計測します")
+async def ping(interaction: discord.Interaction):
+	ping = client.latency
+	cpu_percent = psutil.cpu_percent()
+	mem = psutil.virtual_memory() 
+	embed = discord.Embed(title="Ping", description=f"Ping : {ping}ms\nCPU : {cpu_percent}%\nMemory : {mem.percent}%", color=discord.Colour.gold())
+	embed.set_thumbnail(url=client.user.display_avatar.url)
+	await interaction.response.send_message(embed=embed)
 
 # 60秒に一回ループ
 @tasks.loop(seconds=60)
