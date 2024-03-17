59a31804b6b86555d9c0f45517b9a1f2ed72deb3
nennneko5787 <nennneko5787@gmail.com>
2024-03-17 17:46:09 +0900

    メッセージ系

--- main.py
+++ main.py
@@ -14,13 +14,24 @@ JIHOU_CHANNEL_ID = 1208730354656084008 #チャンネルID
 
 @client.event
 async def setup_hook():
-    loop.start()
+	loop.start()
 
 @client.event
 async def on_ready():
-    await tree.sync()
-    print("起動!")
-    change.start()
+	await tree.sync()
+	print("起動!")
+	change.start()
+
+@client.event
+async def on_message(message):
+	if message.author.bot:
+		return
+	if message.content == 'おはよう':
+		await message.channel.send('> `おはよう…うももも…ももも…ももももも…&*@!!?&...`', silent=True)
+	elif message.content == 'よろしく':
+		await message.channel.send('`よろしくお願いしまスた!`', silent=True)
+	elif message.content == 'おやすみ':
+		await message.channel.send('https://pbs.twimg.com/media/Fj8xGYAakAAYkyy?format=png&name=small', silent=True)
 
 @tree.command(name="ping", description="pingを計測します")
 async def ping(interaction: discord.Interaction):
@@ -33,58 +44,58 @@ async def ping(interaction: discord.Interaction):
 
 @tree.command(name="genshin_userinfo", description="UIDからユーザーの情報を確認できます")
 async def ping(interaction: discord.Interaction, uid: str):
-    await interaction.response.defer()
-    async with aiohttp.ClientSession() as session:
-        async with session.get(f'https://enka.network/api/uid/{uid}') as response:
-            if response.status != 404:
-                user = await response.json()
-            else:
-                embed = discord.Embed(title="ユーザーが見つかりませんでした。", description="UIDが間違っていないか、確認してください。")
-                await interaction.followup.send()
-    embed = discord.Embed(title=f"{user['playerInfo']['nickname']} の情報",description=f"レベル: **{user['playerInfo']['level']}**\n自己紹介: \n```\n{user['playerInfo']['signature']}\n```")
-    await interaction.followup.send(embed=embed)
+	await interaction.response.defer()
+	async with aiohttp.ClientSession() as session:
+		async with session.get(f'https://enka.network/api/uid/{uid}') as response:
+			if response.status != 404:
+				user = await response.json()
+			else:
+				embed = discord.Embed(title="ユーザーが見つかりませんでした。", description="UIDが間違っていないか、確認してください。")
+				await interaction.followup.send()
+	embed = discord.Embed(title=f"{user['playerInfo']['nickname']} の情報",description=f"レベル: **{user['playerInfo']['level']}**\n自己紹介: \n```\n{user['playerInfo']['signature']}\n```")
+	await interaction.followup.send(embed=embed)
 
 # チャンネル入退室時の通知処理
 @client.event
 async def on_voice_state_update(member, before, after):
-    # チャンネルへの入室ステータスが変更されたとき（ミュートON、OFFに反応しないように分岐）
-    if before.channel != after.channel:
-        # 通知メッセージを書き込むテキストチャンネル（チャンネルIDを指定）
-        botRoom = client.get_channel(1208724530445422592)
+	# チャンネルへの入室ステータスが変更されたとき（ミュートON、OFFに反応しないように分岐）
+	if before.channel != after.channel:
+		# 通知メッセージを書き込むテキストチャンネル（チャンネルIDを指定）
+		botRoom = client.get_channel(1208724530445422592)
 
-        # 入退室を監視する対象のボイスチャンネル（チャンネルIDを指定）
-        announceChannelIds = [1209040270553784330, 1218143184778039357, 1218151477395329064, 1218151616725909606, 1218151648732647594]
+		# 入退室を監視する対象のボイスチャンネル（チャンネルIDを指定）
+		announceChannelIds = [1209040270553784330, 1218143184778039357, 1218151477395329064, 1218151616725909606, 1218151648732647594]
 
-        now = int(datetime.now(ZoneInfo("Asia/Tokyo")).timestamp())
+		now = int(datetime.now(ZoneInfo("Asia/Tokyo")).timestamp())
 
-        #if before.channel is None:
-        #    msg = f'> `{now:%m/%d-%H:%M}` に `{member.name}` が `{after.channel.name}` に`参加`しまシたでシ。'
-        #    await alert_channel.send(msg)
-        #elif after.channel is None:
-        #    msg = f'> `{now:%m/%d-%H:%M}` に `{member.name}` が `{before.channel.name}` から`退出`しまスた。'
-        #    await alert_channel.send(msg)
+		#if before.channel is None:
+		#	msg = f'> `{now:%m/%d-%H:%M}` に `{member.name}` が `{after.channel.name}` に`参加`しまシたでシ。'
+		#	await alert_channel.send(msg)
+		#elif after.channel is None:
+		#	msg = f'> `{now:%m/%d-%H:%M}` に `{member.name}` が `{before.channel.name}` から`退出`しまスた。'
+		#	await alert_channel.send(msg)
 
-        # 退室通知
-        if before.channel is not None and before.channel.id in announceChannelIds:
-            await botRoom.send(f"**<t:{now}:f>**に、**<#{before.channel.id}>**から、__{member.mention}__が`退出`しまスた。", silent=True)
-        # 入室通知
-        if after.channel is not None and after.channel.id in announceChannelIds:
-            await botRoom.send(f"**<t:{now}:f>**に、**<#{after.channel.id}>** に、__{member.mention}__が`参加`しまシたでシ。", silent=True)
+		# 退室通知
+		if before.channel is not None and before.channel.id in announceChannelIds:
+			await botRoom.send(f"**<t:{now}:f>**に、**<#{before.channel.id}>**から、__{member.mention}__が`退出`しまスた。", silent=True)
+		# 入室通知
+		if after.channel is not None and after.channel.id in announceChannelIds:
+			await botRoom.send(f"**<t:{now}:f>**に、**<#{after.channel.id}>** に、__{member.mention}__が`参加`しまシたでシ。", silent=True)
 
 @tasks.loop(seconds=20)
 async def change():
-    status = discord.Status.online
-    game = discord.Game(name="幻塔")
-    await client.change_presence(status=status, activity=game)
+	status = discord.Status.online
+	game = discord.Game(name="幻塔")
+	await client.change_presence(status=status, activity=game)
 
 # 60秒に一回ループ
 @tasks.loop(seconds=60)
 async def loop():
-    # 現在の時刻
-    now = datetime.now(ZoneInfo("Asia/Tokyo")).strftime('%H:%M')
-    if now == '00:00':
-        channel = client.get_channel(JIHOU_CHANNEL_ID)
-        await channel.send('https://cdn.discordapp.com/attachments/1208773839023247390/1216853798086246471/AP1GczNYgiHoE-mNro7tzg-rB65rTJ-CfPC98xw2hDvNFzRx6RDVs794Xee_Www1200-h675-s-no.png?ex=6601e669&is=65ef7169&hm=bc216034890394dc50482395cb8b776b5bf7f6257da343a50b8133abff2b739a&')
+	# 現在の時刻
+	now = datetime.now(ZoneInfo("Asia/Tokyo")).strftime('%H:%M')
+	if now == '00:00':
+		channel = client.get_channel(JIHOU_CHANNEL_ID)
+		await channel.send('https://cdn.discordapp.com/attachments/1208773839023247390/1216853798086246471/AP1GczNYgiHoE-mNro7tzg-rB65rTJ-CfPC98xw2hDvNFzRx6RDVs794Xee_Www1200-h675-s-no.png?ex=6601e669&is=65ef7169&hm=bc216034890394dc50482395cb8b776b5bf7f6257da343a50b8133abff2b739a&')
 
 keep_alive()
 client.run(os.getenv("discord"))
